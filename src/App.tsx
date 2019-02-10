92043c9d84f0d396bd562e48f95feef4bdd25791
Keith Fung <keith.robert.fung@gmail.com>
2019-02-10 02:18:41 -0500

    Add Theming for Material UI
    Remove App.css
    Add Theme to be used for Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,33 @@ import './App.css';
 import BookBorrowApi from './api/BookBorrowApi';
 import MockApi from './api/MockApi';
 import BookList from './components/BookList';
+import {
+  WithStyles,
+  withStyles,
+  Theme,
+  createStyles
+} from '@material-ui/core/styles';
 
-class App extends Component {
+const styles = (theme: Theme) => createStyles({
+  app: {
+    textAlign: 'center',
+  },
+});
+
+interface Props extends WithStyles<typeof styles> {
+}
+
+class App extends Component<Props> {
   private api: BookBorrowApi = new MockApi;
 
   render() {
+    const { classes } = this.props;
     return (
-      <div className="App">
+      <div className={classes.app}>
         <BookList books={this.api.getBooks()}/>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
